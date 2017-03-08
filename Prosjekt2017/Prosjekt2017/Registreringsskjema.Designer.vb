244c1d8ac75fd04029122d51209cea32c6f5d8e8
kietng <kiet.nguyenth95@gmail.com>
2017-03-08 13:06:49 +0100

    Login og registrering(database)

--- Prosjekt2017/Prosjekt2017/Registreringsskjema.Designer.vb
+++ Prosjekt2017/Prosjekt2017/Registreringsskjema.Designer.vb
@@ -30,11 +30,17 @@ Partial Class Registreringsskjema
         Me.lblPoststed = New System.Windows.Forms.Label()
         Me.txtFornavn = New System.Windows.Forms.TextBox()
         Me.txtEtternavn = New System.Windows.Forms.TextBox()
-        Me.txtFødselsdato = New System.Windows.Forms.TextBox()
+        Me.txtFodselsdato = New System.Windows.Forms.TextBox()
         Me.txtAdresse = New System.Windows.Forms.TextBox()
         Me.txtPostnummer = New System.Windows.Forms.TextBox()
         Me.txtPoststed = New System.Windows.Forms.TextBox()
         Me.btnRegistrer = New System.Windows.Forms.Button()
+        Me.txtTlf = New System.Windows.Forms.TextBox()
+        Me.lbTlf = New System.Windows.Forms.Label()
+        Me.txtBrukernavn = New System.Windows.Forms.TextBox()
+        Me.txtPassord = New System.Windows.Forms.TextBox()
+        Me.lbBrukernavn = New System.Windows.Forms.Label()
+        Me.lbPassord = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblFornavn
@@ -105,12 +111,12 @@ Partial Class Registreringsskjema
         Me.txtEtternavn.Size = New System.Drawing.Size(100, 20)
         Me.txtEtternavn.TabIndex = 7
         '
-        'txtFødselsdato
+        'txtFodselsdato
         '
-        Me.txtFødselsdato.Location = New System.Drawing.Point(105, 66)
-        Me.txtFødselsdato.Name = "txtFødselsdato"
-        Me.txtFødselsdato.Size = New System.Drawing.Size(100, 20)
-        Me.txtFødselsdato.TabIndex = 8
+        Me.txtFodselsdato.Location = New System.Drawing.Point(105, 66)
+        Me.txtFodselsdato.Name = "txtFodselsdato"
+        Me.txtFodselsdato.Size = New System.Drawing.Size(100, 20)
+        Me.txtFodselsdato.TabIndex = 8
         '
         'txtAdresse
         '
@@ -135,23 +141,77 @@ Partial Class Registreringsskjema
         '
         'btnRegistrer
         '
-        Me.btnRegistrer.Location = New System.Drawing.Point(265, 172)
+        Me.btnRegistrer.Location = New System.Drawing.Point(288, 289)
         Me.btnRegistrer.Name = "btnRegistrer"
         Me.btnRegistrer.Size = New System.Drawing.Size(97, 41)
         Me.btnRegistrer.TabIndex = 12
         Me.btnRegistrer.Text = "Registrer"
         Me.btnRegistrer.UseVisualStyleBackColor = True
         '
+        'txtTlf
+        '
+        Me.txtTlf.Location = New System.Drawing.Point(288, 66)
+        Me.txtTlf.Name = "txtTlf"
+        Me.txtTlf.Size = New System.Drawing.Size(100, 20)
+        Me.txtTlf.TabIndex = 13
+        '
+        'lbTlf
+        '
+        Me.lbTlf.AutoSize = True
+        Me.lbTlf.Location = New System.Drawing.Point(235, 69)
+        Me.lbTlf.Name = "lbTlf"
+        Me.lbTlf.Size = New System.Drawing.Size(43, 13)
+        Me.lbTlf.TabIndex = 14
+        Me.lbTlf.Text = "Telefon"
+        '
+        'txtBrukernavn
+        '
+        Me.txtBrukernavn.Location = New System.Drawing.Point(105, 286)
+        Me.txtBrukernavn.Name = "txtBrukernavn"
+        Me.txtBrukernavn.Size = New System.Drawing.Size(100, 20)
+        Me.txtBrukernavn.TabIndex = 15
+        '
+        'txtPassord
+        '
+        Me.txtPassord.Location = New System.Drawing.Point(105, 312)
+        Me.txtPassord.Name = "txtPassord"
+        Me.txtPassord.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassord.TabIndex = 16
+        '
+        'lbBrukernavn
+        '
+        Me.lbBrukernavn.AutoSize = True
+        Me.lbBrukernavn.Location = New System.Drawing.Point(41, 289)
+        Me.lbBrukernavn.Name = "lbBrukernavn"
+        Me.lbBrukernavn.Size = New System.Drawing.Size(62, 13)
+        Me.lbBrukernavn.TabIndex = 17
+        Me.lbBrukernavn.Text = "Brukernavn"
+        '
+        'lbPassord
+        '
+        Me.lbPassord.AutoSize = True
+        Me.lbPassord.Location = New System.Drawing.Point(54, 315)
+        Me.lbPassord.Name = "lbPassord"
+        Me.lbPassord.Size = New System.Drawing.Size(45, 13)
+        Me.lbPassord.TabIndex = 18
+        Me.lbPassord.Text = "Passord"
+        '
         'Registreringsskjema
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(417, 227)
+        Me.ClientSize = New System.Drawing.Size(422, 388)
+        Me.Controls.Add(Me.lbPassord)
+        Me.Controls.Add(Me.lbBrukernavn)
+        Me.Controls.Add(Me.txtPassord)
+        Me.Controls.Add(Me.txtBrukernavn)
+        Me.Controls.Add(Me.lbTlf)
+        Me.Controls.Add(Me.txtTlf)
         Me.Controls.Add(Me.btnRegistrer)
         Me.Controls.Add(Me.txtPoststed)
         Me.Controls.Add(Me.txtPostnummer)
         Me.Controls.Add(Me.txtAdresse)
-        Me.Controls.Add(Me.txtFødselsdato)
+        Me.Controls.Add(Me.txtFodselsdato)
         Me.Controls.Add(Me.txtEtternavn)
         Me.Controls.Add(Me.txtFornavn)
         Me.Controls.Add(Me.lblPoststed)
@@ -175,9 +235,15 @@ Partial Class Registreringsskjema
     Friend WithEvents lblPoststed As Label
     Friend WithEvents txtFornavn As TextBox
     Friend WithEvents txtEtternavn As TextBox
-    Friend WithEvents txtFødselsdato As TextBox
+    Friend WithEvents txtFodselsdato As TextBox
     Friend WithEvents txtAdresse As TextBox
     Friend WithEvents txtPostnummer As TextBox
     Friend WithEvents txtPoststed As TextBox
     Friend WithEvents btnRegistrer As Button
+    Friend WithEvents txtTlf As TextBox
+    Friend WithEvents lbTlf As Label
+    Friend WithEvents txtBrukernavn As TextBox
+    Friend WithEvents txtPassord As TextBox
+    Friend WithEvents lbBrukernavn As Label
+    Friend WithEvents lbPassord As Label
 End Class
