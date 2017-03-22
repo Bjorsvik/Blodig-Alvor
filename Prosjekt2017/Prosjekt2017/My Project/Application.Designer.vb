ff027fe22e4d36ca1a4def52f1036d12859d5512
kietng <kiet.nguyenth95@gmail.com>
2017-03-22 15:01:19 +0100

    YO WASSUUP

--- Prosjekt2017/Prosjekt2017/My Project/Application.Designer.vb
+++ Prosjekt2017/Prosjekt2017/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.Ansattside
+            Me.MainForm = Global.WindowsApplication1.Hjemmeside
         End Sub
     End Class
 End Namespace
