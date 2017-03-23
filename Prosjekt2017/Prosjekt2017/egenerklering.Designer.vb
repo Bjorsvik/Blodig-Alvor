b7295a5ea00a08ec878b68dbdae1aff2d9dfcff4
audiskvist <flageaudun@gmail.com>
2017-03-23 11:49:23 +0100

    ordnet label1

--- Prosjekt2017/Prosjekt2017/egenerklering.Designer.vb
+++ Prosjekt2017/Prosjekt2017/egenerklering.Designer.vb
@@ -398,9 +398,9 @@ Partial Class egenerklering
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(199, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(329, 29)
+        Me.Label1.Size = New System.Drawing.Size(346, 29)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Velkommen til blodbanken!"
+        Me.Label1.Text = "Spørreskjema for blodgivere"
         '
         'Label2
         '
