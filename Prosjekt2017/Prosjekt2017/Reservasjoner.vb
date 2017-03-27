c4c302becf0e1f2b0a3c76375db727673e7e852e
TormodLien <tormod.h.lien@gmail.com>
2017-03-27 12:33:23 +0200

    Reservasjonsklasse

---
+++ Prosjekt2017/Prosjekt2017/Reservasjoner.vb
@@ -0,0 +1,20 @@
+﻿Public Class Reservasjoner
+
+    Dim db As New Database()
+    Dim ansatt As New Ansatt()
+    Dim blodgiver As New Blodgiver()
+
+
+    Public Sub oppdater()
+        db.Query("DELETE * from Reservasjoner WHERE dato < GETDATE()-1")
+    End Sub
+
+    Public Function getAlleReservasjoner() As DataTable
+        Return db.Query("SELECT * from Reservasjoner")
+    End Function
+
+    Public Sub reserver()
+
+    End Sub
+
+End Class
