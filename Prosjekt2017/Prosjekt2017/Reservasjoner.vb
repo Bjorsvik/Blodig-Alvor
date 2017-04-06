45b00de88c8294bb014e706a189a7b13ad5846a9
kietng <kiet.nguyenth95@gmail.com>
2017-04-06 10:28:10 +0200

    yoyo fuckers

--- Prosjekt2017/Prosjekt2017/Reservasjoner.vb
+++ Prosjekt2017/Prosjekt2017/Reservasjoner.vb
@@ -31,6 +31,9 @@ Where Tidspunkt.tidspunkt = Reservasjon.tidspunkt
 And dato = '" & resDato & "') >=5
 Group by Tidspunkt.tidspunkt")
     End Function
+    Public Function getLastResIDByPersonID(personID As Integer) As DataTable
+        Return db.Query("SELECT MAX(resID) As resID FROM Reservasjon JOIN Person ON Reservasjon.personID = Person.personID WHERE Person.personID = '" & personID & "'")
+    End Function
 
     Public Sub reserver(ByVal dato As String, ByVal personID As Integer, ByVal tid As String)
 
