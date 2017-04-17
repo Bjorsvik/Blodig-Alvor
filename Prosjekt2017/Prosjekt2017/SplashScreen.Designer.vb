6fc37c125faab32b80754295a3b9da8c205e45ab
TormodLien <tormod.h.lien@gmail.com>
2017-04-17 22:30:23 +0200

    Fikset ek og karantene
    svett

---
+++ Prosjekt2017/Prosjekt2017/SplashScreen.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SplashScreen))
+        Me.pbLoad = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblProsent = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbLoad
+        '
+        Me.pbLoad.Location = New System.Drawing.Point(114, 418)
+        Me.pbLoad.Name = "pbLoad"
+        Me.pbLoad.Size = New System.Drawing.Size(447, 23)
+        Me.pbLoad.TabIndex = 1
+        '
+        'Timer1
+        '
+        '
+        'lblProsent
+        '
+        Me.lblProsent.AutoSize = True
+        Me.lblProsent.BackColor = System.Drawing.Color.Transparent
+        Me.lblProsent.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProsent.Location = New System.Drawing.Point(517, 357)
+        Me.lblProsent.Name = "lblProsent"
+        Me.lblProsent.Size = New System.Drawing.Size(0, 31)
+        Me.lblProsent.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ErrorImage = Nothing
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(75, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(516, 409)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'SplashScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(719, 444)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblProsent)
+        Me.Controls.Add(Me.pbLoad)
+        Me.Name = "SplashScreen"
+        Me.Text = "SplashScreen"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pbLoad As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblProsent As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
